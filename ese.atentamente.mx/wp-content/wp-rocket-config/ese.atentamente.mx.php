b102e12528b8163acf025f62d56afe861d9d203c
root <root@ip-172-31-30-13.us-west-2.compute.internal>
2018-05-12 15:57:50 +0000

    weekly commit

---
+++ ese.atentamente.mx/wp-content/wp-rocket-config/ese.atentamente.mx.php
@@ -0,0 +1,16 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+$rocket_cookie_hash = 'ca1f5ea6179497277624e29509de7ca1';
+$rocket_secret_cache_key = '5af6e00a0c524532915763';
+$rocket_cache_mobile = '1';
+$rocket_cache_reject_uri = '(.*)/feed/?|/checkout/(.*)|/cart/|/my-account/(.*)|/wc-api/v(.*)|/wp-json/(.*)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array (
+);
+$rocket_cache_ssl = '1';
+$rocket_do_caching_mobile_files = '0';
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array (
+);
